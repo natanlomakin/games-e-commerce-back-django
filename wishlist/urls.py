7d5d66f717a5380a96988e6bf9122afed872cdca
natan lumkin <NATANLUM@rafael.co.il>
2022-08-29 16:41:39 +0300

    second commit

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -3,5 +3,11 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    
+    path('getuserwishlist/', views.showUserWishlistGames),
+    path('addtouserwishlist/', views.addGameToUserWishlist),
+    path('deltefromuserwishlist/', views.deleteGameFromUserWishlist),
+
+
 ]
+
+
